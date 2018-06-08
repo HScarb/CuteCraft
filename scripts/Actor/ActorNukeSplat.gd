dee79a0730a2d38891caf744972cb0d6f3e8895f
ScarbMac <jjhfen00@163.com>
2018-06-08 22:48:59 +0800

    FinalPPT 基本完成，核弹泼溅图修改

--- scripts/Actor/ActorNukeSplat.gd
+++ scripts/Actor/ActorNukeSplat.gd
@@ -7,6 +7,6 @@ func play_model_animation(effect_tree_node):
         return
     if not check_name(effect_tree_node):
         return
-    var new_model = add_model_at_location(effect_tree_node, model, at_location, false, Global.MAP_LAYER_UNIT)
+    var new_model = add_model_at_location(effect_tree_node, model, at_location, false, Global.MAP_LAYER_GROUND)
     model_instance = new_model
     return new_model
